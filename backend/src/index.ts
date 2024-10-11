c66f07c26acbd7040c065d218ebb9c17ad957b3b
tanmay <kuamrchaurasiatanmay@gmail.com>
2024-10-11 21:38:52 +0530

    connected prisma

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,23 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
+app.post('/api/signup', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.post('/api/signin', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.post('/api/blog', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.put('/api/blog', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.get('/api/blog/:id', (c) => {
   return c.text('Hello Hono!')
 })
 
