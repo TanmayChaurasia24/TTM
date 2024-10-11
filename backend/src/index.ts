cde45202b802240ec321af5504d15c081045eafe
tanmay <kuamrchaurasiatanmay@gmail.com>
2024-10-11 23:10:20 +0530

    mm20

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,28 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/extension'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: String
+  }
+}>()
 
-app.post('/api/signup', (c) => {
+app.post('/api/signup', async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate)
+
+  const body = await c.req.json();
+
+  const created_user = await prisma.user.create({
+    data: {
+      email: body.email,
+      password: body.password,
+    }
+  })
+
+
   return c.text('Hello Hono!')
 })
 
